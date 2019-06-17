fffc93cb8008b33eca50caa3ef9584a9babb0095
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-06-17 13:22:13 +0300

    Initial project setup

---
+++ image-depth-z-buffer/image-depth-z-buffer/BundleFiles/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  image-depth-z-buffer
+//
+//  Created by Serg Liamthev on 6/17/19.
+//  Copyright © 2019 serglam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootVC = ViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ image-depth-z-buffer/image-depth-z-buffer/UserStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image-depth-z-buffer
+//
+//  Created by Serg Liamthev on 6/17/19.
+//  Copyright © 2019 serglam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
